1cc976174ab87a2f1c65c65bbc6cf5e4a2dd9daa
oOthkOo <tierry.danquin@gmail.com>
2013-06-29 00:27:58 +0200

    New commit

--- bin/supervizer.js
+++ bin/supervizer.js
@@ -3,88 +3,249 @@
 const SPZ_SUCCESS_EXIT = 0;
 const SPZ_ERROR_EXIT   = 1;
 
-var fs = require('fs');
+/**
+ * Require all modules
+ */
+var common = require('../lib/common');
+var main = require('../lib/main');
 var path = require('path');
-var util = require('util');
-var cliTable = require('cli-table');
-var watch = require('watch');
-var package = require('../package.json');
-var settings = require('../settings.json');
+var Table = require('cli-table');
 var commander = require('commander');
+var request = require('request');
 
-function spz_error( message ) {
-	console.log('\n\033[31m  [ERROR]: ' + message + '\x1B[39m');
+/**
+ * Setting all modules
+ */
+
+/*request.defaults( {
+	json: true
+});*/
+
+/**
+ * Settings all global variables
+ */
+var serverHost = common.settings.host + ':' + common.settings.port + '/';
+
+/**
+ * All private functions
+ */
+function showError( message ) {
+	console.log('\n\033[31m  [ERROR]: ' + message + '\x1B[39m\n');
 }
 
-function spz_info( message ) {
-	console.log('\n\033[34m  [INFO]: ' + message + '\x1B[39m');
+function showHttpError( code ) {
+	var message = '';
+	switch (code) {
+		case 500:
+			message = '(Network:' + code + ') - Internal server error occured.';
+			break;
+		case 401:
+			message = '(Auth:' + code + ') - Invalid username or password.';
+			break;
+		case 404:
+			message = '(System:' + code + ') - Invalid command for this server.';
+			break;
+		case 700:
+			message = '(Network:' + code + ') - Unable to connect to the server.';
+			break;
+		default:
+			message = '(System:' + code + ') - An unknow error occured.';
+			break;
+	}	
+	showError( message );
 }
 
-var procs = [];
+function showInfo( message ) {
+	console.log('\n\033[34m  [INFO]: ' + message + '\x1B[39m\n');
+}
 
-var np = {
-	id: '1',
-	name: 'hello-web',
-	group: 'main',
-	uid: 'hello-web',
-	gid: 'hello-web',
-	script: '/var/lib/node/hello-web/server.js',
-	watch: {
-		interval: 2000,
-		path: '/var/lib/node/hello-web',
-		excludes: [
-			'logs',
-			'public',
-			'build',
-			'scripts'
-		]
-	},
-	files: {
-		pid: '/var/lib/node/hello-web/server.pid',
-		log: '/var/lib/node/hello-web/server.log'
-	},
-	host: 'http://localhost',
-	port: 3200,
-	pid: '',
-	status: 'down',
-	stats: {
-		started: 0,
-		crashed: 0,
-		stopped: 0,
-		restarted: 0,
-		memory: 0,
-		cpu: 0
+function isQueryValid(error, response, body) {
+	
+	if (!response) {
+		showHttpError( 700 );
+		return null;
+	}
+	if (error || response.statusCode != 200) {
+		showHttpError( response.statusCode );
+		return null;
+	}
+	
+	var query = body || false;
+	if (!query) {
+		showHttpError( 701 );
+		return null;
+	}
+	
+	if (query.result != common.results.SPZ_OK) {		
+		showError( query.error );		
+		return null;
+	}	
+	
+	return query;
+}
+
+function getAppPattern() {	
+	return {
+		id: '',
+		name: commander.name || 'untitled',
+		group: commander.group || 'main',
+		uid: commander.run ? commander.run.split(':')[0] : '',
+		gid: commander.run ? commander.run.split(':')[1] : '',
+		script: commander.script || '',
+		watch: {
+			enabled: commander.watch ? true : false,
+			interval: commander.interval || 2000,
+			path: commander.watch || '',
+			excludes: commander.exclude || []
+		},
+		files: {
+			pid: commander.pid || '',
+			log: commander.log || ''
+		},
+		host: commander.host || 'http://localhost',
+		port: commander.port || '',
+		pid: '',
+		status: 'down',
+		stats: {
+			started: 0,
+			crashed: 0,
+			stopped: 0,
+			restarted: 0,
+			memory: 0,
+			cpu: 0
+		}
 	}
 }
 
-commander.version(package.version)
+function getRequestParams( uri, data ) {
+	return {
+		url: serverHost + uri,
+		headers: { 'Content-type': 'application/json' },
+		body: data,
+		json: true
+	}
+}
+
+function listFormat( type, value ) {
+	switch (type) {
+		case 'script':
+			return value ? path.basename(value) : 'N/C';
+		case 'memory':
+			return getHumanBytes( value || 0, 2 );
+		case 'uptime':
+			return value || 'N/C';
+		case 'pid':
+			return value || 'N/C';
+		case 'host':
+			return value ? value.replace('http://',''): 'N/C';
+		case 'status':
+			return value;
+		case 'port':
+			return value || 'N/C';
+		default:
+			return value;
+	}
+	return '';
+}
+
+function showAppList( apps ) {
+	
+	if ( !apps || apps.length < 1 ) {
+		console.log( 'No apps found.');
+		return;
+	}
+	
+	var table = new Table({
+	    head: ['id', 'name', 'pid','script', 'group', 'status', 'host', 'port', 'mem', 'uptime'],
+	    colWidths: [5, 15, 8, 15, 10, 8, 20, 9, 8, 8]
+	    
+	});
+	
+	for (var a=0; a<apps.length; a++) {
+		var app = apps[a];
+		table.push([
+		     listFormat('id', app.id),
+		     listFormat('name', app.name),
+		     listFormat('pid', app.pid),
+		     listFormat('script', app.script),
+		     listFormat('group', app.group),
+		     listFormat('status', app.status),
+		     listFormat('host', app.host),
+		     listFormat('port', app.port),
+		     listFormat('memory', app.stats.memory),
+		     listFormat('uptime', app.stats.uptime)
+		]);
+	}
+
+	console.log(table.toString());
+}
+
+function getHumanBytes(bytes, precision) {
+	var kilobyte = 1024;
+	var megabyte = kilobyte * 1024;
+	var gigabyte = megabyte * 1024;
+	var terabyte = gigabyte * 1024;
+
+	if ((bytes >= 0) && (bytes < kilobyte)) {
+		return bytes + ' B';
+	}
+	else if ((bytes >= kilobyte) && (bytes < megabyte)) {
+	    return (bytes / kilobyte).toFixed(precision) + ' KB';
+	}
+	else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+	    return (bytes / megabyte).toFixed(precision) + ' MB';
+	}
+	else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+		return (bytes / gigabyte).toFixed(precision) + ' GB';
+	}
+	else if (bytes >= terabyte) {
+		return (bytes / terabyte).toFixed(precision) + ' TB';
+	}
+	else {
+	    return bytes + ' B';
+	}
+};
+
+/**
+ * Setting all commands
+ */
+commander.version(common.pkg.version)
 	.option('-v --verbose', 'display verbose data')
 	.option('-f --force', 'force supervizer actions')
-	.option('-n --name <string>', 'specify process name')
-	.option('-u --user <string>', 'specify process username')
-	.option('-g --group <string>', 'specify process group')
-	.option('-s --script <path>', 'specify process main script')
-	.option('-l --log <file>', 'specify process log output file')
-	.option('-t --pid <file>', 'specify process pid file')
+	.option('-n --name <string>', 'specify app name')
+	.option('-r --run <user:group>', 'specify user to run app')
+	.option('-g --group <string>', 'specify app group')
+	.option('-s --script <path>', 'specify app main script')
+	.option('-l --log <file>', 'specify app log output file')
+	.option('-t --pid <file>', 'specify app pid file')
 	.option('-w --watch <path>', 'specify path to watch')
 	.option('-i --interval <milliseconds>', 'specify interval in milliseconds for watch')
 	.option('-e --exclude <path>', 'specify path to exclude')
 	.option('-h --host <address>', 'specify host to bind')
 	.option('-p --port <port>', 'specify port to bind')
+	.option('-a --auth <user:password>', 'specify user/password to use')
 	.option('-c --config <file>', 'specify config file to load')
 	.usage('[command] <options>');
+
+commander.command('help <command>')
+.description('print required/allowed options for each command.')
+.action(function() {
+	
+});
 
 commander.command('install')
 	.description('install supervizer as daemon')
 	.action(function() {
+		
+		if (process.getuid() != 0) {
+			spz_error('You must run supervizer as root for this command.');
+			process.exit(SPZ_ERROR_EXIT);
+		}		
 });
 
 commander.command('load')
 	.description('load from a process configuration json file')
 	.action(function() {
-
-	console.log('verbose: ' + (commander.verbose ? 'yes' : 'no'));
-	console.log('config: ' + commander.config);
 });
 
 commander.command('save')
@@ -94,7 +255,22 @@ commander.command('save')
 
 commander.command('add')
 	.description('add a node process')
-	.action(function() {
+	.action(function() {	
+		
+		var app = getAppPattern();		
+		var params = getRequestParams( 'apps', JSON.stringify(app) );
+		
+		console.log( '[send]:\n' + ' - url: ' + params.url + '\n - data: ' + JSON.stringify(app) + '\n' );
+		
+		request.post( params, function(error, response, body){
+			console.log( '[receive]:\n - data: ' + JSON.stringify(body));
+			
+			var query = isQueryValid(error, response, body);
+			if (!query) {
+				process.exit(SPZ_ERROR_EXIT);
+			}
+			
+		});
 });
 
 commander.command('remove')
@@ -135,6 +311,23 @@ commander.command('restartAll')
 commander.command('list')
 	.description('list all node process')
 	.action(function() {
+		
+	var app = getAppPattern();	
+	var params = getRequestParams( 'apps/list', JSON.stringify(app) );
+	
+	console.log( '[send]:\n' + ' - url: ' + params.url + '\n - data: ' + JSON.stringify(app) + '\n' );
+	
+	request.post( params, function(error, response, body){
+		console.log( '[receive]:\n - data: ' + JSON.stringify(body));
+		
+		var query = isQueryValid(error, response, body);
+		if (!query) {
+			process.exit(SPZ_ERROR_EXIT);
+		}
+		showAppList( query.data );
+		
+	});
+		
 });
 
 commander.command('monit')
@@ -180,6 +373,3 @@ if (process.argv.length == 2) {
 
 //console.log( 'process.argv.length: ' + process.argv.length );
 commander.parse(process.argv);
-
-
-
